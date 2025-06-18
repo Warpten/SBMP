965aefc87184663d67e43013a7c818613d1fe2c6
Warpten <vertozor@gmail.com>
2025-06-18 05:10:30 +0000

    Split source projects

--- xmake.lua
+++ xmake.lua
@@ -1,18 +1,27 @@
 add_rules("plugin.vsxmake.autoupdate")
-set_allowedplats("windows")
+-- set_allowedplats("windows")
 set_allowedmodes("debug", "releasedbg", "release", "coverage")
 set_defaultmode("debug")
-add_requires("libllvm", {configs = {clang = true}})
+add_requires("libllvm", { configs = { clang = true } })
+
+-- Global rules
+set_languages("c++20")
+set_exceptions("cxx")
+set_encodings("utf-8")
 
 target("SBMP", function()
   set_kind("shared")
-  set_languages("c++20")
-  set_exceptions("cxx")
-  set_encodings("utf-8")
 
-  add_files("src/*.cpp")
-  add_headerfiles("src/*.hpp")
-  add_includedirs("src/**")
+  add_files("src/sbmp/**.cpp")
+  add_headerfiles("src/sbmp/**.hpp")
+  add_includedirs("src/sbmp")
+end)
+
+target("Generator", function()
+  set_kind("binary")
+  add_files("src/generator/*.cpp")
+  add_headerfiles("src/generator/**.hpp")
+  add_includedirs("src/generator")
 
   add_packages("libllvm")
 end)
